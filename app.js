c40cfbc30668a8d169e644e90570c837b777d89a
Haseeb <chhaseebamjad0@gmail.com>
2024-03-10 12:58:03 +0500

    Complete the hambugur menu

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const hamburger = document.body.querySelector('.hamburger');
+const mobLinks = document.body.querySelector('.mob_nav_links');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('active');
+    mobLinks.classList.toggle('active');
 });
