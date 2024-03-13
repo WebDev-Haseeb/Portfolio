76d370076fd79866058545c868a27911426cd539
Haseeb <chhaseebamjad0@gmail.com>
2024-03-13 19:59:41 +0500

    Make scrollbar hidden for screens below 700px

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ const mobLinks = document.body.querySelector('.mob_nav_links');
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('active');
     mobLinks.classList.toggle('active');
+    scrollY = hidden;
 });
